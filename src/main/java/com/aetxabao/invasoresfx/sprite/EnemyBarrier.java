daee2433182756eaf5e31549349248aafec4cc51
AaronJVC1996 <avasquecar1@educacion.navarra.es>
2024-04-06 17:56:16 +0200

    Estoy intentando hacer que mi enemybarrier cree asteroides cada 5 ataques recibidos con la interfaz ICanSpawn, salen con la misma imagen que enemybarrier y no con la de obstaculo, mañana investigare como soluciono esto

--- src/main/java/com/aetxabao/invasoresfx/sprite/EnemyBarrier.java
+++ src/main/java/com/aetxabao/invasoresfx/sprite/EnemyBarrier.java
@@ -3,52 +3,57 @@ package com.aetxabao.invasoresfx.sprite;
 import com.aetxabao.invasoresfx.util.Rect;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static com.aetxabao.invasoresfx.game.AppConsts.*;
 import static com.aetxabao.invasoresfx.game.AppConsts.ENEMYSHIP_ALFA;
 
-public class EnemyBarrier extends AEnemy implements IHaveShield{
+public class EnemyBarrier extends AEnemy implements IHaveShield, ICanSpawn {
     private int escudoEspacial;
     int N;//ticks para cambio de frame
     int n;
     Rect gameRect;
+
     public EnemyBarrier(Rect gameRect, Image img, int N) {
         super(img, ENEMYSHIP_ROWS, 1);
         this.gameRect = gameRect;
         xSpeed = ENEMYSHIP_MAX_SPEED;
-        x = gameRect.left+width/2;
+        x = gameRect.left + width / 2;
         this.N = N;
         this.n = 0;
-        this.escudoEspacial = 20;
+        this.escudoEspacial = 21;
     }
 
-    public void updateFrame(){
-        if (++n==N) {
+    public void updateFrame() {
+        if (++n == N) {
             n = 0;
             currentFrame = ++currentFrame % cols;
         }
     }
 
     @Override
-    public Rect getRect(){
-        return new Rect(x, y, (int)(x + ENEMYSHIP_ALFA * width),(int)(y + ENEMYSHIP_ALFA * height));
+    public Rect getRect() {
+        return new Rect(x, y, (int) (x + ENEMYSHIP_ALFA * width), (int) (y + ENEMYSHIP_ALFA * height));
     }
 
     @Override
     public void update() {
-            if (x > gameRect.right - width - xSpeed || x + xSpeed < gameRect.left) {
-                xSpeed = -xSpeed;
-            }
-            x = x + xSpeed;
-            y = y + ySpeed;
-            updateFrame();
+        if (x > gameRect.right - width - xSpeed || x + xSpeed < gameRect.left) {
+            xSpeed = -xSpeed;
         }
+        x = x + xSpeed;
+        y = y + ySpeed;
+        updateFrame();
+    }
 
     @Override
     public void draw(GraphicsContext gc) {
         int srcX = currentFrame * width;
         int srcY = 0;
         Rect src = new Rect(srcX, srcY, srcX + width, srcY + height);
-        Rect dst = new Rect(x, y, (int)(x + ENEMYSHIP_ALFA * width), (int)(y + ENEMYSHIP_ALFA * height));
+        Rect dst = new Rect(x, y, (int) (x + ENEMYSHIP_ALFA * width), (int) (y + ENEMYSHIP_ALFA * height));
         gc.drawImage(img, src.left, src.top, src.width(), src.height(),
                 dst.left, dst.top, dst.width(), dst.height());
     }
@@ -65,4 +70,19 @@ public class EnemyBarrier extends AEnemy implements IHaveShield{
         }
     }
 
+    @Override
+    // todo Aqui me quedo para cuano despierte seguir, de momento se crean los enemigos cada 5 ataques pero
+    // se crean con la misma imagen que el boss y no con la imagen de el asteroide, tengo que investigar un poco
+    public List<AEnemy> spawn() {
+            List<AEnemy> enemies = new ArrayList<>();
+            if (escudoEspacial % 5 == 0) {
+                for (int i = 0; i < 3; i++) {
+                              Obstaculo obstaculo = new Obstaculo(gameRect, img, N);
+                    obstaculo.setPos(i +2 , 0);
+                    enemies.add(obstaculo); //
+                }
+            }
+            return enemies;
+
+    }
 }
